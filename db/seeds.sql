f5d63da66b6a3bdd271b1b9e35f39ed9657ffc47
Carlos Hernandez <carlos@devcodify.com>
2020-03-10 19:47:25 -0600

    create fle server.js schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO cats (burger_name) VALUES ('cheeseburger');
+INSERT INTO cats (burger_name) VALUES ('popular steak');
+INSERT INTO cats (burger_name) VALUES ('baconator');
+INSERT INTO cats (burger_name) VALUES ('chickenmix');
