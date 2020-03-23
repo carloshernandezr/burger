d9234b8bb2292af86da4b057791a87200a9231e0
Carlos Hernandez <carlos@devcodify.com>
2020-03-23 01:53:31 -0600

    modal box

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,7 +1,7 @@
 console.log("javsxcript loaded")
 
 $("#add").on("click", function () {
-  
+
     event.preventDefault()
     var newBurger = {
         burger_name: $("#bu").val(),
@@ -21,18 +21,60 @@ $("#add").on("click", function () {
 })
 
 
-$(".eatburger").on("click", function(){
+// $(".eatburger").on("click", function(){
 
-    var id= $(this).attr("data-id")
-    console.log("id", id)
-    $.ajax({
-        type: "put",     // GET POST PUT DELETE
-        url: "/api/burgers/" + id ,
-    
-        success: function (response) {
-            console.log(response)
-            location.reload()
-        }
+//     var id= $(this).attr("data-id")
+//     console.log("id", id)
+//     $.ajax({
+//         type: "put",     // GET POST PUT DELETE
+//         url: "/api/burgers/" + id ,
+
+//         success: function (response) {
+//             console.log(response)
+//             location.reload()
+//            // $("#dialog").dialog();
+//         }
+//     });
+
+
+// })
+
+ 
+ 
+    $('.eatburger').click(function () {
+        
+      //  $('#dialog').dialog('open');
+     // alert("Yummy")
+   
+     $('.eatburger').modal('show')
+        var id = $(this).attr("data-id")
+        console.log("id", id)
+        $.ajax({
+            type: "put",     // GET POST PUT DELETE
+            url: "/api/burgers/" + id,
+
+            success: function (response) {
+                console.log(response)
+                location.reload()
+                // $("#dialog").dialog();
+            }
     });
+});
 
-})
+// $("#dialogg" ).dialog();
+// $('#isabel').click(function(){
+
+//     $("#dialogg" ).dialog();
+
+// })
+
+
+$('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-title').text('New message to ' + recipient)
+    modal.find('.modal-body input').val(recipient)
+  })
