e24021834f3b3ea4b32464ac180735954334d1ac
Carlos Hernandez <carlos@devcodify.com>
2020-03-23 01:54:25 -0600

    box

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var exphbs = require("express-handlebars");
 var mysql = require("mysql");
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
