d71fdbee879e2d10b66127d2f16a91291b1712a7
Carlos Hernandez <carlos@devcodify.com>
2020-03-23 01:55:15 -0600

    clean code

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -21,31 +21,10 @@ $("#add").on("click", function () {
 })
 
 
-// $(".eatburger").on("click", function(){
-
-//     var id= $(this).attr("data-id")
-//     console.log("id", id)
-//     $.ajax({
-//         type: "put",     // GET POST PUT DELETE
-//         url: "/api/burgers/" + id ,
-
-//         success: function (response) {
-//             console.log(response)
-//             location.reload()
-//            // $("#dialog").dialog();
-//         }
-//     });
-
-
-// })
-
- 
  
     $('.eatburger').click(function () {
         
-      //  $('#dialog').dialog('open');
-     // alert("Yummy")
-   
+
      $('.eatburger').modal('show')
         var id = $(this).attr("data-id")
         console.log("id", id)
@@ -61,19 +40,10 @@ $("#add").on("click", function () {
     });
 });
 
-// $("#dialogg" ).dialog();
-// $('#isabel').click(function(){
-
-//     $("#dialogg" ).dialog();
-
-// })
-
 
 $('#exampleModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) // Button that triggered the modal
-    var recipient = button.data('whatever') // Extract info from data-* attributes
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var button = $(event.relatedTarget) 
+    var recipient = button.data('whatever') 
     var modal = $(this)
     modal.find('.modal-title').text('New message to ' + recipient)
     modal.find('.modal-body input').val(recipient)
